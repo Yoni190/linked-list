3f840243c1a66c0a579896e2453520a537fdbcc4
yoni290 <yfisuhu@gmail.com>
2025-06-21 09:30:07 +0300

    Add node constructor

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 class LinkedList {
-    append(value){
 
+    list = {}
+
+    append(value){
+        node = new Node(value)
     }
 }
 
 class Node {
     value = null
     nextNode = null
+
+    constructor(value, nextNode=null){
+        this.value = value
+        this.nextNode = nextNode
+    }
 }
 
 
