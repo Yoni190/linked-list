7ef4f4c184291faa8e16650bb1048003491a1974
yoni290 <yfisuhu@gmail.com>
2025-06-22 22:27:03 +0300

    Create prepend function

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 class LinkedList {
 
-    list = {}
+    list = []
 
     append(value){
-        node = new Node(value)
+        const node = new Node(value, null)
+        this.list.push(node)
+    }
+
+    prepend(value){
+        let pointer = null
+
+        if(this.list.length > 0){
+            pointer = this.list[0]
+        }
+        
+        const node = new Node(value, pointer)
+        this.list.unshift(node)
     }
 }
 
@@ -11,11 +23,17 @@ class Node {
     value = null
     nextNode = null
 
-    constructor(value, nextNode=null){
+    constructor(value, nextNode){
         this.value = value
         this.nextNode = nextNode
     }
 }
+
+const list = new LinkedList()
+list.append(2)
+list.prepend(1)
+
+console.log(list.list)
 
 
 
